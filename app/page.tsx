e8bfdd87cf3c18145a09ab9098d8268385d107df
AdemMlayah <adammlayah140@gmail.com>
2024-10-24 03:14:53 +0100

    home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import BlogPreview from "./components/BlogPreview/BlogPreview";
+import TopicCarousel from "./components/TopicCarousel/TopicCarousel";
 
 export default function Home() {
   return (
-    <div className="">
-     
+    <div className=" max-w-[1350px] w-full mx-auto h-60 flex transition-gap gap-0 md:gap-4 lg:gap-8 xl:gap-16 justify-center items-center my-5">
+      <div className="max-w-[820px] flex flex-col w-full mx-auto ">
+        <TopicCarousel/>
+        <BlogPreview />
+      </div>
+      <div className="w-[350px] hidden h-screen md:block border-l">
+      </div>
     </div>
   );
 }
